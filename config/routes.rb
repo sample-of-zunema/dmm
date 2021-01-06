0706e98b7aefeddba1127c1442c447574351b7b8
zunema <yxxa.abc@gmail.com>
2021-01-06 13:03:53 +0000

    usersコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/unsubscribe'
+  get 'users/withdraw'
   get 'homes/top'
   get 'homes/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
