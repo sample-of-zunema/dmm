34f3269de6f3f19c18d98a52d371a4b84c716bb9
zunema <yxxa.abc@gmail.com>
2021-01-06 12:36:39 +0000

    Commentモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_120209) do
+ActiveRecord::Schema.define(version: 2021_01_06_123454) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "dream_id"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dreams", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "comment_id"
+    t.datetime "day"
+    t.integer "emotion"
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
