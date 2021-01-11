7487eeb795fef83f37a1cf43ebb2b6902c7ec171
zunema <yxxa.abc@gmail.com>
2021-01-11 03:50:30 +0000

    Favoriteモデルの作成/アソシエーション

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -2,6 +2,11 @@ class Dream < ApplicationRecord
 
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 
   enum emotion: {楽しい: 0, 嫌気: 1, 悲しい: 2, 恐い: 3, 怒り: 4}
 
