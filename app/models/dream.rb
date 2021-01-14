0e297214221082777bc9c796a4a89cecb89e9cce
zunema <yxxa.abc@gmail.com>
2021-01-14 04:22:46 +0000

    Devise-i18nのgem追加

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -7,6 +7,10 @@ class Dream < ApplicationRecord
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
+  
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :start_time, presence: true
 
   enum emotion: {楽しい: 0, 嫌気: 1, 悲しい: 2, 恐い: 3, 怒り: 4}
 
