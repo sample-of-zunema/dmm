4d482cbe24c0fff8e7b39c822a4f9c5eda46bf83
zunema <yxxa.abc@gmail.com>
2021-01-18 12:34:58 +0000

    ActibAdminのgemインストール

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,20 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
     def after_sign_in_path_for(resource)
-      dreams_top_path
+      if resource.class == AdminUser
+        admin_root_path
+      else
+        dreams_top_path
+      end
     end
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
-      devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+      if user_signed_in?
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+        devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+      end
     end
 
 end
