4617f5de8000b018d2e6a984fd0c5de293ea3052
zunema <yxxa.abc@gmail.com>
2021-01-19 12:13:23 +0000

    問い合わせ機能の更新

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,7 +1,7 @@
 class InquiryMailer < ApplicationMailer
-  
+
   default from: "example@example.com"
-  
+
   def received_email(inquiry)
     @inquiry = inquiry
     mail(:to => inquiry.email, :subject => 'お問い合わせを承りました')
