66af78d8da41a8734aea26bd9134d6c92bcfb151
zunema <yxxa.abc@gmail.com>
2021-01-20 14:45:24 +0000

    モデルのコメント

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,17 +1,21 @@
 class Dream < ApplicationRecord
 
+  # user/comments/favoritesをそれぞれアソシエーション
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  # いいねをしているかチェック
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
 
+  # 投稿時にtitle/body/start_timeが空でないかバリデーションチェック
   validates :title, presence: true
   validates :body, presence: true
   validates :start_time, presence: true
 
+  # emotion（感情）の分類
   enum emotion: {楽しい: 0, 嫌気: 1, 悲しい: 2, 恐い: 3, 怒り: 4}
 
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,5 +1,6 @@
 class Favorite < ApplicationRecord
 
+  # user/dreamのアソシエーション
   belongs_to :user
   belongs_to :dream
 
