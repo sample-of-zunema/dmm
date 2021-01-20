d934883c97ada03dc29eab88f9f6ee487453ba71
zunema <yxxa.abc@gmail.com>
2021-01-20 12:10:25 +0000

    新規投稿の更新

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,16 @@ module DreamSharing
     # Initialize configuration defaults for originally generated Rails version.
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    # デフォルトの言語設定
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
+    # 言語ファイルを階層ごとに設定するための記述
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    # アプリケーションが対応している言語のホワイトリスト(ja = 日本語, en = 英語)
+    config.i18n.available_locales = %i(ja en)
+    # 上記の対応言語以外の言語が指定された場合、エラーとするかの設定
+    config.i18n.enforce_available_locales = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
