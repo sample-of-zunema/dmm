1384330925f28e159f98fc92972728c0c2b3e413
zunema <yxxa.abc@gmail.com>
2021-01-30 05:42:58 +0000

    写真の更新

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -70,15 +70,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # 拡張子が同じでないとGIFをJPGとかにコンバートできないので、ファイル名を変更
-  def filename
-    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
-  end
+  # def filename
+  #   super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
+  # end
 
   # ファイル名を日付にするとタイミングのせいでサムネイル名がずれる
   # ファイル名はランダムで一意になる
-  # def filename
-  #   "#{secure_token}.#{file.extension}" if original_filename.present?
-  # end
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
 
   protected
 
